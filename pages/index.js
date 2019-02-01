cfbdb6e8878ddeb61366abf5d740797edad61bc0
Andrzej Dąbrowski <andrzej@ratio.team>
2019-02-01 13:03:53 +0100

    video

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Page  from "../src/Page/Page";
 import Article from "../src/Article/Article";
+import ReactPlayer from 'react-player'
 
 function Index() {
     return (
@@ -8,10 +9,25 @@ function Index() {
                 <h1>Dzień dobry</h1>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                 <h2>Let's get deeper</h2>
+                <p>Lorem ipsum dolor sit amet, <code>consectetur</code> adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <ul>
+                    <li>Pies</li>
+                    <li>Kot</li>
+                    <li>Krowa</li>
+                </ul>
+                <p>
+                    <strong>Important note, lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</strong>
+                </p>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
-            
+                <div className={"video"}>
+                    <ReactPlayer url='https://vimeo.com/299612182'  />
+                </div>
+
+                <h2>Let's get deeper</h2>
+                <p>Lorem ipsum dolor sit amet, <code>consectetur</code> adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
             </Article>
         </Page>
     );
--- src/Page/Page.js
+++ src/Page/Page.js
@@ -4,7 +4,8 @@ import { rslin } from "../Layout/ResponsiveStyles";
 import Spacings, { menuHeight } from "../../config/spacings.config";
 
 const Root = styled.div`
-    ${menuHeight.add(rslin(Spacings.m7, Spacings.d7)).css('margin-top')}
+    ${menuHeight.add(rslin(Spacings.m6, Spacings.d7)).css('margin-top')}
+    ${rslin(Spacings.m6, Spacings.d7).css('margin-bottom')}
 `;
 
 const Page = (props) => <Root style={{height: "100%"}}>
