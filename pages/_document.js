cbfbe1c5b695222e685a868de36e7442dc1dd4f6
Andrzej Dąbrowski <andrzej@ratio.team>
2019-02-01 11:26:09 +0100

    fix

--- pages/_document.js
+++ pages/_document.js
@@ -1,18 +1,18 @@
-import Document from 'next/document'
+import Document, { Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
     static async getInitialProps (ctx) {
-        const sheet = new ServerStyleSheet()
-        const originalRenderPage = ctx.renderPage
+        const sheet = new ServerStyleSheet();
+        const originalRenderPage = ctx.renderPage;
 
         try {
             ctx.renderPage = () =>
                 originalRenderPage({
                     enhanceApp: App => props => sheet.collectStyles(<App {...props} />)
-                })
+                });
 
-            const initialProps = await Document.getInitialProps(ctx)
+            const initialProps = await Document.getInitialProps(ctx);
             return {
                 ...initialProps,
                 styles: [...initialProps.styles, ...sheet.getStyleElement()]
@@ -20,5 +20,19 @@ export default class MyDocument extends Document {
         } finally {
             sheet.seal()
         }
+    }
+
+    render() {
+        return (
+            <html>
+            <Head>
+                <link rel="stylesheet" type="text/css" href="/static/normalize.css"></link>
+            </Head>
+            <body>
+            <Main />
+            <NextScript />
+            </body>
+            </html>
+        )
     }
 }
